8539030abeca5d56a2c23a2e763920b7f3ce42fb
Sergio Rodriguez <srodrig0209@gmail.com>
2013-07-19 08:54:14 +0000

    hw3-part3 write ssjs headless grader

--- grader.js
+++ grader.js
@@ -26,6 +26,7 @@ var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var rest = require('restler');
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -64,11 +65,32 @@ var clone = function(fn) {
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-f, --file <html_file>', 'Path to index.html')
+        .option('-u, --url <url>', 'index.html url')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    var path = "index_web.html";
+    if (program.file) {
+        var checkJson = checkHtmlFile(program.file, program.checks);
+        var outJson = JSON.stringify(checkJson, null, 4);
+        console.log(outJson);
+    } 
+    else if (program.url) {
+        rest.get(program.url).on('complete', function(response) {
+	    if (response instanceof Error) {
+	        console.error("Error " + response.message);
+  		return 2;
+	    } else {
+		fs.writeFileSync(path, response);	
+                var checkJson = checkHtmlFile(path, program.checks);
+                var outJson = JSON.stringify(checkJson, null, 4);
+                console.log(outJson);
+	    }
+	})
+    } 
+    else {
+	console.error("Please pass in a file or a url");
+	return 1;
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
