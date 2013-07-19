4e2495bfc541fe686adb9591a3dba23ac03dd22b
Sergio Rodriguez <srodrig0209@gmail.com>
2013-07-19 04:02:38 +0000

    read from index.html

--- web.js
+++ web.js
@@ -3,7 +3,9 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var fs = require('fs');
+  var fileContents = fs.readFileSync("index.html", "utf-8");
+  response.send(fileContents);
 });
 
 var port = process.env.PORT || 5000;
